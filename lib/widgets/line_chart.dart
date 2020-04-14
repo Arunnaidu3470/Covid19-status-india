4a619997192c8ced6900aefc44527d05fba2dea8
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-04-14 22:56:38 +0530

    updated charts

--- lib/widgets/line_chart.dart
+++ lib/widgets/line_chart.dart
@@ -1,4 +1,5 @@
 import 'package:app/api/covid19.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +10,8 @@ class ConfirmedCasesChart extends StatefulWidget {
 }
 
 class _ConfirmedCasesChartState extends State<ConfirmedCasesChart> {
+  int selectedValue = 0;
+
   Covid19Api api = Covid19Api();
   List<Color> gradientColors = [
     const Color(0xff23b6e6),
@@ -22,25 +25,53 @@ class _ConfirmedCasesChartState extends State<ConfirmedCasesChart> {
       padding: const EdgeInsets.all(8),
       margin: const EdgeInsets.all(10),
       width: MediaQuery.of(context).size.width,
-      child: ClipRRect(
-        child: Consumer<TimeSeriesModel>(builder: (cxt, snapshot, _) {
-          if (snapshot == null)
-            return Center(child: CircularProgressIndicator());
-          return LineChart(avgData(snapshot.casesTimeSeries));
-        }),
+      height: 350,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: ClipRRect(
+              child: Consumer<TimeSeriesModel>(builder: (cxt, snapshot, _) {
+                if (snapshot == null)
+                  return Center(child: CircularProgressIndicator());
+                return LineChart(avgData(snapshot.casesTimeSeries));
+              }),
+            ),
+          ),
+          Positioned(
+              bottom: 5,
+              child: Container(
+                width: 300,
+                height: 50,
+                child: CupertinoSegmentedControl<int>(
+                    unselectedColor: Colors.transparent,
+                    selectedColor: Colors.white12,
+                    borderColor: Colors.blue,
+                    padding: const EdgeInsets.all(8),
+                    groupValue: selectedValue,
+                    children: {
+                      0: Text('All', style: TextStyle(color: Colors.white70)),
+                      1: Text('30-days',
+                          style: TextStyle(color: Colors.white70)),
+                      2: Text('7-days', style: TextStyle(color: Colors.white70))
+                    },
+                    pressedColor: Colors.teal.withOpacity(0.5),
+                    onValueChanged: (value) {
+                      setState(() {
+                        selectedValue = value;
+                      });
+                    }),
+              ))
+        ],
       ),
     );
   }
 
   LineChartData avgData(List<CasesTimeSeries> data) {
-    List<FlSpot> spotsConfirmedCasesX = [];
-
-    for (int i = 0; i < data.length; i++) {
-      spotsConfirmedCasesX
-          .add(FlSpot(i.toDouble(), data[i].totalConfirmed / 1000));
-    }
     return LineChartData(
-      lineTouchData: LineTouchData(enabled: true),
+      lineTouchData: LineTouchData(
+          enabled: true,
+          touchTooltipData: LineTouchTooltipData(tooltipBgColor: Colors.black)),
       gridData: FlGridData(
         show: true,
         drawHorizontalLine: true,
@@ -66,11 +97,7 @@ class _ConfirmedCasesChartState extends State<ConfirmedCasesChart> {
               color: Color(0xff68737d),
               fontWeight: FontWeight.bold,
               fontSize: 10),
-          getTitles: (value) {
-            if (value % 30 == 0)
-              return data[value.toInt()].date.substring(0, 6);
-            return '';
-          },
+          getTitles: (value) => _titlesBottom(value, data),
           margin: 8,
         ),
         leftTitles: SideTitles(
@@ -80,11 +107,7 @@ class _ConfirmedCasesChartState extends State<ConfirmedCasesChart> {
             fontWeight: FontWeight.bold,
             fontSize: 10,
           ),
-          getTitles: (value) {
-            int dataY = data[data.length - 1].totalConfirmed ~/ 2000;
-            if (value % dataY == 0) return '$value k';
-            return ' ';
-          },
+          getTitles: (value) => _titlesLeft(value, data),
           reservedSize: 28,
           margin: 12,
         ),
@@ -93,28 +116,17 @@ class _ConfirmedCasesChartState extends State<ConfirmedCasesChart> {
           show: true,
           border: Border.all(color: const Color(0xff37434d), width: 1)),
       minX: 0,
-      maxX: data.length.toDouble() + 10,
+      maxX: _maxXAxisValue(data),
       minY: 0,
-      maxY: data[data.length - 1].totalConfirmed.toDouble() / 500,
+      maxY: _maxYAxisValue(data),
       lineBarsData: [
+        //Confirmed
         LineChartBarData(
-          spots: spotsConfirmedCasesX, //todo
-          // [
-          //   FlSpot(0, 10.44),
-          //   FlSpot(3, 3.44),
-          //   FlSpot(4.9, 3.44),
-          //   FlSpot(6.8, 3.44),
-          //   FlSpot(8, 3.44),
-          //   FlSpot(9.5, 3.44),
-          //   FlSpot(11, 3.44),
-          //   FlSpot(20, 10.44),
-          // ],
+          spots: _spotsConfirmedCasesx(data), //todo
           isCurved: true,
           colors: [
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                .lerp(0.2),
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                .lerp(0.2),
+            ColorTween(begin: Colors.red, end: Colors.redAccent).lerp(0.2),
+            ColorTween(begin: Colors.red, end: Colors.redAccent).lerp(0.2),
           ],
           barWidth: 5,
           isStrokeCapRound: true,
@@ -122,15 +134,145 @@ class _ConfirmedCasesChartState extends State<ConfirmedCasesChart> {
             show: false,
           ),
           belowBarData: BarAreaData(show: true, colors: [
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
+            ColorTween(begin: Colors.red, end: Colors.redAccent)
                 .lerp(0.2)
                 .withOpacity(0.1),
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
+            ColorTween(begin: Colors.red, end: Colors.redAccent)
+                .lerp(0.2)
+                .withOpacity(0.1),
+          ]),
+        ),
+        //Recovered
+        LineChartBarData(
+          spots: _spotsRecoveryCasesx(data),
+          isCurved: true,
+          colors: [
+            ColorTween(begin: Colors.green, end: Colors.greenAccent).lerp(0.2),
+            ColorTween(begin: Colors.green, end: Colors.greenAccent).lerp(0.2),
+          ],
+          barWidth: 4,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(show: true, colors: [
+            ColorTween(begin: Colors.green, end: Colors.greenAccent)
+                .lerp(0.2)
+                .withOpacity(0.1),
+            ColorTween(begin: Colors.red, end: Colors.redAccent)
+                .lerp(0.2)
+                .withOpacity(0.1),
+          ]),
+        ),
+        //Recovered
+        LineChartBarData(
+          spots:
+              // spotsConfirmedCasesX, //todo
+              _spotsDeceasedCasesx(data),
+          isCurved: true,
+          colors: [
+            ColorTween(begin: Colors.white, end: Colors.white).lerp(0.2),
+            ColorTween(begin: Colors.white, end: Colors.white).lerp(0.2),
+          ],
+          barWidth: 4,
+          isStrokeCapRound: true,
+          dotData: FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(show: true, colors: [
+            ColorTween(begin: Colors.white, end: Colors.white)
+                .lerp(0.2)
+                .withOpacity(0.1),
+            ColorTween(begin: Colors.white, end: Colors.white)
                 .lerp(0.2)
                 .withOpacity(0.1),
           ]),
         ),
       ],
     );
+  }
+
+  List<FlSpot> _spotsRecoveryCasesx(List<CasesTimeSeries> data) {
+    List<FlSpot> list = [];
+    if (selectedValue == 1) {
+      data = data.sublist(data.length - 31);
+    } else if (selectedValue == 2) {
+      data = data.sublist(data.length - 7);
+    }
+    for (int i = 0; i < data.length; i++) {
+      list.add(FlSpot(i.toDouble(), data[i].totalRecovered / 1000));
+    }
+    return list;
+  }
+
+  List<FlSpot> _spotsDeceasedCasesx(List<CasesTimeSeries> data) {
+    List<FlSpot> list = [];
+    if (selectedValue == 1) {
+      data = data.sublist(data.length - 31);
+    } else if (selectedValue == 2) {
+      data = data.sublist(data.length - 7);
+    }
+    for (int i = 0; i < data.length; i++) {
+      list.add(FlSpot(i.toDouble(), data[i].totalDeceased / 1000));
+    }
+    return list;
+  }
+
+  List<FlSpot> _spotsConfirmedCasesx(List<CasesTimeSeries> data) {
+    List<FlSpot> list = [];
+
+    if (selectedValue == 1) {
+      data = data.sublist(data.length - 31);
+    } else if (selectedValue == 2) {
+      data = data.sublist(data.length - 7);
+    }
+    for (int i = 0; i < data.length; i++) {
+      list.add(FlSpot(i.toDouble(), data[i].totalConfirmed / 1000));
+    }
+
+    return list;
+  }
+
+  double _maxXAxisValue(List<CasesTimeSeries> data) {
+    if (selectedValue == 2) return 7;
+    return _spotsConfirmedCasesx(data).length.toDouble() + 10;
+  }
+
+  double _maxYAxisValue(List<CasesTimeSeries> data) {
+    // if (selectedValue == 1)
+    //   return data[data.length - 1].dailyConfirmed.toDouble() / 100;
+    if (selectedValue == 2)
+      return data[data.length - 1].dailyConfirmed.toDouble() / 80;
+    return data[data.length - 1].totalConfirmed.toDouble() / 600;
+  }
+
+  String _titlesLeft(double value, List<CasesTimeSeries> data) {
+    int dataY = data[data.length - 1].totalConfirmed ~/ 2000;
+    if (value % dataY == 0) return '$value k';
+    return ' ';
+  }
+
+  String _titlesBottom(double value, List<CasesTimeSeries> data) {
+    //7-days(week),30-days(month),all(whole data)
+    // print(data.sublist(data.length - 8)[0]);
+    if (selectedValue == 2) {
+      if (value < 7) {
+        return data
+            .sublist(data.length - 7)[value.toInt()]
+            .date
+            .substring(0, 6);
+      }
+    }
+    if (selectedValue == 1) {
+      if (value < 31 && value % 5 == 0) {
+        return data
+            .sublist(data.length - 31)[value.toInt()]
+            .date
+            .substring(0, 6);
+      }
+    }
+
+    if (value % 30 == 0) return data[value.toInt()].date.substring(0, 6);
+    return '';
   }
 }
